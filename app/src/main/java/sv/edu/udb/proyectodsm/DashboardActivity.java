750a0810858e6a23b1b64a642e3e778a00cde36d
batres <cbatresaleman97@gmail.com>
2021-05-01 11:13:14 -0600

    detalles

--- app/src/main/java/sv/edu/udb/proyectodsm/DashboardActivity.java
+++ app/src/main/java/sv/edu/udb/proyectodsm/DashboardActivity.java
@@ -2,7 +2,9 @@ package sv.edu.udb.proyectodsm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+    }
+
+    public void citas (View v) {
+        Intent i=new Intent(this, CitasActivity.class);
+        startActivity(i);
+    }
+    public void pacientes (View v) {
+        Intent i=new Intent(this, ClientesActivity.class);
+        startActivity(i);
+    }
+    public void inventario (View v) {
+        Intent i=new Intent(this, ActivityInventario.class);
+        startActivity(i);
+    }
+    public void doctores (View v) {
+        Intent i=new Intent(this, MedicosActivity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/sv/edu/udb/proyectodsm/MainActivity.java
+++ app/src/main/java/sv/edu/udb/proyectodsm/MainActivity.java
@@ -73,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             Toast.makeText(getApplicationContext(), "Acceso Exitoso!", Toast.LENGTH_LONG).show();
                             progressBar.setVisibility(View.GONE);
-                            Intent intent = new Intent(MainActivity.this, ActivityInventario.class);
+                            Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
                          //   intent.putExtra("txtcorreo", emailTV.getText().toString());
                             startActivity(intent);
                         }
